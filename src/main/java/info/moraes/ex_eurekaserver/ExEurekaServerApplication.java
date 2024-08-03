5d8cf63f0fc7a91c4ded36ff54067da5ce09f3fa
Leonardo <leonardobastosmoraes@hotmail.com>
2024-08-03 18:03:35 -0300

    Eureka Server

---
+++ src/main/java/info/moraes/ex_eurekaserver/ExEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package info.moraes.ex_eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ExEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExEurekaServerApplication.class, args);
+    }
+
+}
